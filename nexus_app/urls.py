d1ff1c1b0a61a867308162eda0af3bdab6a3c168
Mauricio-Dias-Silva <mauricio.dias.silva@gmail.com>
2026-03-10 12:22:18 -0300

    feat(init): First commit - Bio-Nexus Django Platform Migration

---
+++ nexus_app/urls.py
@@ -0,0 +1,10 @@
+﻿from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.nexus_index, name='index'),
+    path('transcendence/', views.transcendence, name='transcendence'),
+    path('bioelectric/', views.bioelectric, name='bioelectric'),
+    path('touchless/', views.touchless, name='touchless'),
+    path('biomimicry/', views.biomimicry, name='biomimicry'),
+]
---
+++ nexus_app/views.py
@@ -0,0 +1,16 @@
+﻿from django.shortcuts import render
+
+def nexus_index(request):
+    return render(request, 'nexus_app/index.html')
+
+def transcendence(request):
+    return render(request, 'nexus_app/transcendence.html')
+
+def bioelectric(request):
+    return render(request, 'nexus_app/bioelectric.html')
+
+def touchless(request):
+    return render(request, 'nexus_app/touchless.html')
+
+def biomimicry(request):
+    return render(request, 'nexus_app/biomimicry.html')
